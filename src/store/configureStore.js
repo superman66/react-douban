7cb5d94ca2503b610ee7f384873abb9a182b729b
superman <supermanchc@gmail.com>
2017-04-08 01:45:22 +0800

    Remove logger in production

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,7 +3,10 @@ import thunk from 'redux-thunk'
 import { logger } from 'redux-logger'
 import rootReducer from '../reducers/index' 
 
-const middleware = [thunk, logger];
+const middleware = [thunk];
+if(process.env.NODE_ENV !== 'production'){
+    middleware.push(logger);
+}
 export default function configureStore(initialState){
     return createStore(
         rootReducer,
